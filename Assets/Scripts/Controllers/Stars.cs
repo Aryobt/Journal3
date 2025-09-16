4bfe45f93ae20efd501edc448ecec2c66ac4efed
Aryobt <aryobt2@gmail.com>
2025-09-16 14:55:37 -0400

    tried to work on the stars

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -6,9 +6,12 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    
 
     // Update is called once per frame
     void Update()
     {
+      //  DrawConstellation();
     }
+   // public void DrawConstellation();
 }
