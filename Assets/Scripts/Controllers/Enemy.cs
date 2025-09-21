f3ee9533fb3a6a36b05e78cc42a2102f9ec59009
Aryobt <aryobt2@gmail.com>
2025-09-21 12:08:31 -0400

    worked a little bit on enemy movement

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,25 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float Speed = 0.5f;
+    public float maxX = 5f;
+    public float minX = -5f;
+
+    private Vector3 enemyPosition;
+
     private void Update()
     {
+        enemyMovement();
+        transform.position = Vector3.MoveTowards(transform.position, enemyPosition, Speed * Time.deltaTime);
     }
 
+    public void enemyMovement()
+    {
+        {
+
+        }
+    }
 }
