c0f4bb8ccac42eeda938f37d337e2e9fb45ae238
taghavia <taghavia@OA-S141-06>
2025-09-22 11:53:18 -0400

    working on stars

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
-    public float drawingTime;
+    public float drawingTime = 0.5f;
+    float StartTime = 0f;
     
 
 
@@ -20,13 +21,18 @@ public class Stars : MonoBehaviour
     }
    public void DrawConstellation()
     {
+        
         Vector3 Star1Pos = new Vector3(12, 5, 0);
         Vector3 Star2Pos = new Vector3(16, 1, 0);
         Vector3 Star3Pos = new Vector3(12, -1, 0);
         Vector3 Star4Pos = new Vector3(16, -4, 0);
 
-        Debug.DrawLine(Star1Pos, Star2Pos, Color.white);
-        Debug.DrawLine(Star2Pos, Star3Pos, Color.white);
-        Debug.DrawLine(Star3Pos, Star4Pos, Color.white);
+        float HowLong = StartTime / drawingTime;
+
+        Vector3 Star1To2 = Vector3.Lerp(Star1Pos, Star2Pos, HowLong);
+
+        StartTime = (StartTime + 1) % (drawingTime + 1);
+
+        Debug.DrawLine(Star1Pos,Star2Pos,Color.white);
     }
 }
