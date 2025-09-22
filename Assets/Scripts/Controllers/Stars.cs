ee067a6f85623933e263eb9369498fc8c11181e7
taghavia <taghavia@OA-S141-06>
2025-09-22 10:32:24 -0400

    tried doing stars

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -8,13 +8,16 @@ public class Stars : MonoBehaviour
     public float drawingTime;
     
 
+
     // Update is called once per frame
     void Update()
     {
+      //  Vector3 = new Vector3 Star1Pos(12, 5, 0);
         DrawConstellation();
     }
    public void DrawConstellation()
     {
-
+       
+       // Debug.DrawLine(StarPos , new Vector3 (16,1,0), Color.white);
     }
 }
