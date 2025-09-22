0bce029a55f0fc8e45b7aae5f74ef8c4ebb28b76
taghavia <taghavia@OA-S141-06>
2025-09-22 11:18:48 -0400

    put the draw codes in the void

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -15,18 +15,18 @@ public class Stars : MonoBehaviour
      
         DrawConstellation();
 
-        Vector3 Star1Pos = new Vector3(12, 5, 0);
-        Vector3 Star2Pos = new Vector3(16, 1, 0);
-        Vector3 Star3Pos = new Vector3(12,-1,0);
-        Vector3 Star4Pos = new Vector3(16,-4,0);
-
-        Debug.DrawLine(Star1Pos, Star2Pos, Color.white);
-        Debug.DrawLine(Star2Pos, Star3Pos, Color.white);
-        Debug.DrawLine(Star3Pos, Star4Pos, Color.white);
+       
 
     }
    public void DrawConstellation()
     {
-       
+        Vector3 Star1Pos = new Vector3(12, 5, 0);
+        Vector3 Star2Pos = new Vector3(16, 1, 0);
+        Vector3 Star3Pos = new Vector3(12, -1, 0);
+        Vector3 Star4Pos = new Vector3(16, -4, 0);
+
+        Debug.DrawLine(Star1Pos, Star2Pos, Color.white);
+        Debug.DrawLine(Star2Pos, Star3Pos, Color.white);
+        Debug.DrawLine(Star3Pos, Star4Pos, Color.white);
     }
 }
