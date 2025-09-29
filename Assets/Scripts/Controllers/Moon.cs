681fb08b827f7d1790ec4db272e018e8f9716473
taghavia <taghavia@OA-S141-06>
2025-09-29 12:09:16 -0400

    moon rotates but wrong way

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -7,7 +7,9 @@ public class Moon : MonoBehaviour
     public Transform planetTransform;
 
     public float orbitalSpeed;
-    public int MoonPoints = 6;
+    public float radius;
+
+    private float angle;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,22 +19,29 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       //transform.Rotate(new Vector3(0, 0, orbitalSpeed) * Time.deltaTime);
+        angle += orbitalSpeed * Time.deltaTime;
+        //transform.Rotate(new Vector3(0, 0, orbitalSpeed) * Time.deltaTime);
+        float x = Mathf.Cos(angle) * radius;
+        float z = Mathf.Sin(angle) * radius;
+
+       
+        transform.position = planetTransform.position + new Vector3(x, 0, z);
+        
     }
-    public void OrbitalMotion(float radius, int MoonPoints)
-    {
-        float angleStep = 360f;
-        float radians = angleStep * Mathf.Deg2Rad;
-        List<Vector3> points = new List<Vector3>();
+   // public void OrbitalMotion(float radius, int MoonPoints)
+   // {
+      //  float angleStep = 360f;
+      //  float radians = angleStep * Mathf.Deg2Rad;
+      //  List<Vector3> points = new List<Vector3>();
 
-        for (int i = 0; i < MoonPoints; i++)
-        {
-            float adjustment = radians * i;
-            Vector3 point = new Vector3(Mathf.Cos(radians + adjustment), Mathf.Sin(radians + adjustment)) * radius;
+      //  for (int i = 0; i < MoonPoints; i++)
+      //  {
+       //     float adjustment = radians * i;
+       //     Vector3 point = new Vector3(Mathf.Cos(radians + adjustment), Mathf.Sin(radians + adjustment)) * radius;
 
-            points.Add(point);
+      //      points.Add(point);
 
-        }
-        Vector3 center = transform.position;
-    }
+      //  }
+      //  Vector3 center = transform.position;
+   // }
 }
