15761d5825051e085933392ebc727752f7c2a306
taghavia <taghavia@OA-S141-06>
2025-09-29 10:47:18 -0400

    added moon rotation but its rotating around itself

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -6,15 +6,16 @@ public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
 
+    public float orbitalSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-
+       transform.Rotate(new Vector2(0,orbitalSpeed) * Time.deltaTime);
     }
 }
